c72dac4bd86b7ecd2570d59802abfce13e84b33b
ViAsmit <asmit.vml.962@gmail.com>
2021-07-10 14:31:20 +0530

    gatsby ssr

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "frontend", "public")));
 
 app.use("/", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -39,7 +39,7 @@ app.use("/users", usersRouter);
 app.use("/posts", articleRouter);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "public", "index.html"));
 });
 
 // catch 404 and forward to error handler
